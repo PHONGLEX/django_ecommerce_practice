18ff67d5c30143a3d24d0b6667b3e641c9c2ca19
phonglex <fonglex@gmail.com>
2021-06-30 20:15:57 +0700

    fix db config

--- core/settings/production.py
+++ core/settings/production.py
@@ -24,11 +24,11 @@ AUTH_PASSWORD_VALIDATORS = [
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': config('NAME'),
-        'USER': config('USER'),
-        'PASSWORD': config('PASSWORD'),
-        'HOST': config('HOST'),
-        'PORT': config('PORT')
+        # 'NAME': config('NAME'),
+        # 'USER': config('USER'),
+        # 'PASSWORD': config('PASSWORD'),
+        # 'HOST': config('HOST'),
+        # 'PORT': config('PORT')
     }
 }
 
